72fb6f423a799ab1f892d0b9b4692a4e35457da3
Osaro Iyoha <saro.iyoha@gmail.com>
2020-04-01 06:37:10 +0100

    Fix Randomness

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -2,16 +2,22 @@
 //  ViewController.swift
 //  Magic 8 Ball
 //
-//  Created by Angela Yu on 14/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Angela Yu on 14/06/2019, Edited by Osaro Iyoha 01/04/2020
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")]
+    @IBOutlet weak var ballImageView: UIImageView!
 
+    @IBAction func askButton(_ sender: UIButton) {
+        let randomNumber = Int.random(in: 0...4)
+        print(randomNumber)
+        ballImageView.image = ballArray[randomNumber]
+    }
+    
 
 
 }
